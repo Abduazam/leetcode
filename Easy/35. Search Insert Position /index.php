854369af459e5dba16250e9a0e6b4960e54c4d23
abduazam <abduazamdev@gmail.com>
2024-01-04 23:20:24 +0500

    easy: 35

---
+++ Easy/35. Search Insert Position /index.php
@@ -0,0 +1,34 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param integer[] $nums
+     * @param integer $target
+     * @return integer
+     */
+    public function searchInsert(array $nums, int $target): int
+    {
+        $result = 0;
+
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($target === $nums[$i]) {
+                return $i;
+            }
+
+            if ($target > $nums[$i]) {
+                $result = $i + 1;
+            }
+        }
+
+        return $result;
+    }
+}
+
+$numbers = [1,3,5,6];
+$target = 7;
+
+$solution = new Solution();
+$result = $solution->searchInsert($numbers, $target);
+var_dump($result);
