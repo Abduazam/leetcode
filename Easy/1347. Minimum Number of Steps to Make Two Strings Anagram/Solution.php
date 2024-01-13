2f7e76bf262cd8a7c8b3455bfb376716c00d40bf
abduazam <abduazamdev@gmail.com>
2024-01-13 22:00:22 +0500

    easy: 1347

---
+++ Easy/1347. Minimum Number of Steps to Make Two Strings Anagram/Solution.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Easy\MinimumNumberStepsMakeTwoStringsAnagram;
+
+class Solution
+{
+    /**
+     * @param string $s
+     * @param string $t
+     *
+     * @return integer
+     */
+    public function minSteps(string $s, string $t): int
+    {
+        $charsOfS = array_count_values(str_split($s));
+        $charsOfT = array_count_values(str_split($t));
+
+        $minStep = 0;
+
+        foreach ($charsOfS as $char => $count) {
+            if (array_key_exists($char, $charsOfT)) {
+                $difference = $count - $charsOfT[$char];
+                if ($difference > 0) {
+                    $minStep += $difference;
+                }
+            } else {
+                $minStep += $count;
+            }
+        }
+
+        return $minStep;
+    }
+}
+
+$s = "gctcxyuluxjuxnsvmomavutrrfb";
+$t = "qijrjrhqqjxjtprybrzpyfyqtzf";
+
+
+$solution = new Solution();
+$result = $solution->minSteps($s, $t);
+var_dump($result);
