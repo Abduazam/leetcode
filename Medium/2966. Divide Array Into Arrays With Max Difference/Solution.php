09619bb21def70b7e007cc7df30b08fd9675ad4c
abduazam <abduazamdev@gmail.com>
2024-02-01 23:24:38 +0500

    medium: 2966

---
+++ Medium/2966. Divide Array Into Arrays With Max Difference/Solution.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Medium\DivideArrayIntoArraysWithMaxDifference;
+
+class Solution
+{
+    /**
+     * @param array<integer> $nums
+     * @param integer $k
+     *
+     * @return array<array<integer>>
+     */
+    function divideArray(array $nums, int $k): array
+    {
+        $result = [];
+
+        sort($nums);
+
+        for ($i = 0; $i < count($nums); $i += 3) {
+            if (
+                $nums[$i+2] - $nums[$i+1] <= $k &&
+                $nums[$i+1] - $nums[$i] <= $k &&
+                $nums[$i+2] - $nums[$i] <= $k
+            ) {
+                $result[] = [$nums[$i], $nums[$i+1], $nums[$i+2]];
+            } else {
+                return [];
+            }
+        }
+
+        return $result;
+    }
+}
+
+$nums = [1,3,3,2,7,3];
+
+$solution = new Solution();
+$result = $solution->divideArray($nums, 3);
+var_dump($result);
