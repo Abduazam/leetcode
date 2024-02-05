f47a47ecb2b686066ddb2b9cf827cd9d1899b60d
abduazam <abduazamdev@gmail.com>
2024-02-05 12:10:44 +0500

    easy: 387

---
+++ Easy/387. First Unique Character in a String/Solution.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Easy\FirstUniqueCharacterInString;
+
+class Solution
+{
+    /**
+     * @param string $s
+     *
+     * @return integer
+     */
+    function firstUniqChar(string $s): int
+    {
+        $chars = str_split($s);
+
+        $countChars = array_count_values($chars);
+
+        $key = array_search(1, $countChars);
+
+        if ($key === false) {
+            return -1;
+        } else {
+            return array_search($key, $chars);
+        }
+    }
+}
+
+$string = "loveleetcode";
+
+$solution = new Solution();
+$result = $solution->firstUniqChar($string);
+var_dump($result);
