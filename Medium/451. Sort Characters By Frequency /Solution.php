db714da2b7d2233d12f159ffb3fbe381b22fbbad
abduazam <abduazamdev@gmail.com>
2024-02-07 17:53:45 +0500

    medium: 451

---
+++ Medium/451. Sort Characters By Frequency /Solution.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Medium\SortCharactersByFrequency;
+
+class Solution
+{
+    /**
+     * @param string $s
+     *
+     * @return string
+     */
+    function frequencySort(string $s): string
+    {
+        $chars = count_chars($s, 1);
+        arsort($chars);
+
+        $output = '';
+
+        foreach ($chars as $key => $value) {
+            $output .= str_repeat(chr($key), $value);
+        }
+
+        return $output;
+    }
+}
+
+$string = "Aabb";
+
+$solution = new Solution();
+$result = $solution->frequencySort($string);
+var_dump($result);
