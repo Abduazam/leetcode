4d6d54d74bfcc7e7a0d3dd292ced7d6a19056788
abduazam <abduazamdev@gmail.com>
2024-02-12 00:08:58 +0500

    easy: 278

---
+++ Easy/278. First Bad Version/Solution.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Easy\FirstBadVersion;
+
+class Solution
+{
+    /**
+     * @param integer $n
+     *
+     * @return integer
+     */
+    function firstBadVersion(int $n): int
+    {
+        $first = 1;
+        $latest = $n;
+        $output = PHP_INT_MIN;
+
+        while ($first <= $latest) {
+            $middle = floor(($first + $latest) / 2 );
+
+            if($this->isBadVersion($middle)) {
+                $output = $middle;
+                $latest = $middle - 1;
+            } else {
+                $first = $middle + 1;
+            }
+        }
+
+        return $output;
+    }
+}
