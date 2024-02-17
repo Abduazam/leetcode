1a76b62363a39cde58c8c97c82662b2de9bf1063
abduazam <abduazamdev@gmail.com>
2024-02-17 21:57:53 +0500

    easy: 303

---
+++ Easy/303. Range Sum Query - Immutable /Solution.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Easy\RangeSumQueryImmutable;
+
+class NumArray
+{
+    public array $array;
+
+    /**
+     * @param array<integer> $nums
+     */
+    function __construct(array $nums) {
+        $this->array = $nums;
+    }
+
+    /**
+     * @param integer $left
+     * @param integer $right
+     * @return integer
+     */
+    function sumRange(int $left, int $right): int
+    {
+        return array_sum(array_slice($this->array, $left, ($right - $left) + 1));
+    }
+}
+
+$num = [-2, 0, 3, -5, 2, -1];
+
+$solution = new NumArray($num);
+$r1 = $solution->sumRange(0, 2);
+var_dump($r1);
