d5c7680597569d98bc834e2a6d6cb405f8004ada
abduazam <abduazamdev@gmail.com>
2024-02-23 18:10:40 +0500

    easy: 345

--- Easy/345. Reverse Vowels of a String /Solution.php
+++ Easy/345. Reverse Vowels of a String /Solution.php
@@ -10,6 +10,27 @@ class Solution
      */
     public function reverseVowels(string $s): string
     {
+        $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'U', 'O'];
+        $left = 0;
+        $right = strlen($s) - 1;
+
+        while ($left < $right) {
+            while (! in_array($s[$left], $vowels) && $left < $right) {
+                $left++;
+            }
+
+            while (! in_array($s[$right], $vowels) && $left < $right) {
+                $right--;
+            }
+
+            $temp = $s[$left];
+            $s[$left] = $s[$right];
+            $s[$right] = $temp;
+
+            $left++;
+            $right--;
+        }
+
         return $s;
     }
 }
