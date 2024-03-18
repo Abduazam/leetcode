0207fdda7b8bede6b5bc1b0b254f9bb91e784497
Abduazam <abduazamdev@gmail.com>
2024-03-18 23:00:56 +0500

    easy: 441

---
+++ Easy/441. Arranging Coins/Solution.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Easy\ArrangingCoins;
+
+class Solution
+{
+	/**
+     * @param integer $n
+     * @return integer
+     */
+    public function arrangeCoins(int $n): int
+    {
+        $step = 1;
+        $max = 1;
+
+        while ($n > 0) {
+            if ($n >= $step) {
+                $max = $step;
+            }
+
+            $n -= $step;
+            $step++;
+        }
+
+        return $max;
+    }
+}
+
+$solution = new Solution();
+$result = $solution->arrangeCoins(5);
+var_dump($result);
